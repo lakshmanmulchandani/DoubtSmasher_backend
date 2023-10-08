7f01462c0a7c7d8fb5368b64744deed8cd44e8bd
Lakshman Mulchandani <mulchandanilakshman@gmail.com>
2023-10-08 10:07:28 +0530

    basic boiler plate code is added with folder structure

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+
+const app = express();
+
+app.use(express.json({limit: "30mb", extended: true}));
+app.use(express.urlencoded({limit: "30mb", extended: true}));
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("This is a  API");
+});
+
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`);
+});
