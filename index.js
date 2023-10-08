21148de4cadb458e4317bbda30f9aa5e353ea80c
Lakshman Mulchandani <mulchandanilakshman@gmail.com>
2023-10-08 20:36:25 +0530

    question and answers implemented error in auth.js middleware for pushing question

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ import userRoutes from "./routes/users.js";
 import chatroomRoutes from "./routes/chatroom.js";
 import messageRoutes from "./routes/message.js";
 import topicRoutes from "./routes/topic.js";
+import questionRoutes from "./routes/Questions.js";
+import answerRoutes from "./routes/Answers.js";
 const app = express();
 
 app.use(express.json({limit: "30mb", extended: true}));
@@ -14,7 +16,8 @@ app.use("/user", userRoutes);
 app.use("/chatroom", chatroomRoutes);
 app.use("/message", messageRoutes);
 app.use("/topic", topicRoutes);
-
+app.use("/questions", questionRoutes);
+app.use("/answer", answerRoutes);
 app.get("/", (req, res) => {
   res.send("This is an API");
 });
