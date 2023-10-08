0a975970d460ce393336c59b134d24cafa3da4fc
Lakshman Mulchandani <mulchandanilakshman@gmail.com>
2023-10-08 15:08:05 +0530

    auth corrected

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,23 +5,30 @@ import users from "../models/auth.js";
 
 export const signup = async (req, res) => {
   const {name, email, password} = req.body;
+
   try {
-    const existinguser = await users.findOne({email});
-    if (existinguser) {
-      return res.status(404).json({message: "User already Exist."});
+    console.log(name);
+    if (!password) {
+      return res.status(400).json({message: "Password is required."});
+    }
+
+    const existingUser = await users.findOne({email});
+    if (existingUser) {
+      return res.status(409).json({message: "User already exists."});
     }
 
     const hashedPassword = await bcrypt.hash(password, 12);
     const newUser = await users.create({name, email, password: hashedPassword});
-    // Jwt token is created and sent back to browser as a response in json format
+
+    const JWT_SECRET = "anything";
     const token = jwt.sign(
       {email: newUser.email, id: newUser._id},
-      process.env.JWT_SECRET,
+      JWT_SECRET,
       {expiresIn: "1h"}
     );
-    res.status(200).json({result: newUser, token});
+    res.status(201).json({result: newUser, token});
   } catch (error) {
-    res.status(500).json("Something went worng...");
+    res.status(500).json({message: "Something went wrong..."});
   }
 };
 
@@ -38,9 +45,10 @@ export const login = async (req, res) => {
       return res.status(400).json({message: "Invalid credentials"});
     }
     // Jwt token is created and sent back to browser as a response in json format
+    const JWT_SECRET = "anything";
     const token = jwt.sign(
       {email: existinguser.email, id: existinguser._id},
-      process.env.JWT_SECRET,
+      JWT_SECRET,
       {expiresIn: "1h"}
     );
     res.status(200).json({result: existinguser, token});
